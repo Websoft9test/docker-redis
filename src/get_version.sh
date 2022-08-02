c8db12f61d081073b5404c8d5e43cacb98893c93
qiaofeng1227 <76487013@qq.com>
2022-08-02 16:39:39 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo Redis Version:$(docker exec -i redis /usr/local/bin/redis-server -v) 1>> /data/logs/install_version.txt
+sudo echo Redis Version:$(docker exec -i $1 /usr/local/bin/redis-server -v) 1>> /data/logs/install_version.txt
